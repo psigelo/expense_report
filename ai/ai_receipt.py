11694717d9b31bdbfee4141fde3cb059a572ddbd
Pascal Sigel <pascal.sigel@gmail.com>
2021-11-01 19:04:39 -0300

    minor

--- ai/ai_receipt.py
+++ ai/ai_receipt.py
@@ -5,5 +5,8 @@ import numpy as np
 def get_data_from_area_receipt(area_name, b64_encoded_roi):
     jpg_original = base64.b64decode(b64_encoded_roi)
     img_npy = np.frombuffer(jpg_original, dtype=np.uint8)
+
+    # TODO: implement AI
     print(area_name, img_npy)
-    return "not implemented yet"
+
+    return "not implemented yet!"
