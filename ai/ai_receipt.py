5a4bd152b3b19c3ac6d51501032471d4d34594af
Pascal Sigel <pascal.sigel@gmail.com>
2021-11-28 17:37:10 -0300

    ai receipt now have a direct way to use images of areas not just b64 encoded

--- ai/ai_receipt.py
+++ ai/ai_receipt.py
@@ -2,11 +2,12 @@ import base64
 import numpy as np
 
 
-def get_data_from_area_receipt(area_name, b64_encoded_roi):
+def get_data_from_area_receipt(img_area):
+    return "not implemented yet!"
+
+
+def get_data_from_area_receipt_b64(b64_encoded_roi):
     jpg_original = base64.b64decode(b64_encoded_roi)
     img_npy = np.frombuffer(jpg_original, dtype=np.uint8)
 
-    # TODO: implement AI
-    print(area_name, img_npy)
-
-    return "not implemented yet!"
+    return get_data_from_area_receipt(img_npy)
